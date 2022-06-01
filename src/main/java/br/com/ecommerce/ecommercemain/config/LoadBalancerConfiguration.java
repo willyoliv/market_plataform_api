f0eedbd66188cd7a975dcf87415075152b59c0b8
willyoliv <willyoliversilva@gmail.com>
2022-06-01 12:12:12 -0300

    configurações load balancer

--- src/main/java/br/com/ecommerce/ecommercemain/config/LoadBalancerConfiguration.java
+++ src/main/java/br/com/ecommerce/ecommercemain/config/LoadBalancerConfiguration.java
@@ -5,16 +5,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class LoadBalancerConfiguration {
-    @Bean
-    public ServiceInstanceListSupplier
-    discoveryClientServiceInstanceListSupplier(
-            ConfigurableApplicationContext context) {
-        System.out.println("Configuring Load balancer to prefer same instance");
-        return ServiceInstanceListSupplier.builder()
-                .withBlockingDiscoveryClient()
-                .withSameInstancePreference()
-                .build(context);
-    }
-}
+//@Configuration
+//public class LoadBalancerConfiguration {
+//    @Bean
+//    public ServiceInstanceListSupplier
+//    discoveryClientServiceInstanceListSupplier(
+//            ConfigurableApplicationContext context) {
+//        System.out.println("Configuring Load balancer to prefer same instance");
+//        return ServiceInstanceListSupplier.builder()
+//                .withBlockingDiscoveryClient()
+//                .withSameInstancePreference()
+//                .build(context);
+//    }
+//}
